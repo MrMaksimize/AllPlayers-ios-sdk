dea2978e3f17e542f8d233c0501d7a03447f9a6c
Maksim Pecherskiy <max@maksimize.com>
2012-09-03 11:47:03 -0500

    Basic client and afis coordinator.

---
+++ APIOSAPIClient.m
@@ -0,0 +1,94 @@
+// SongAPIClient.m
+//
+// Copyright (c) 2012 Mattt Thompson (http://mattt.me)
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import "APIOSAPIClient.h"
+
+static NSString * const kAPIOSAPIBaseURLString = @"https://www.allplayers.com/api/v1/rest";
+
+@implementation APIOSAPIClient
+
++ (APIOSAPIClient *)sharedClient {
+    static APIOSAPIClient *_sharedClient = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedClient = [[self alloc] initWithBaseURL:[NSURL URLWithString:kAPIOSAPIBaseURLString]];
+      [_sharedClient setParameterEncoding:AFJSONParameterEncoding];
+    });
+    
+    return _sharedClient;
+}
+
+- (id)initWithBaseURL:(NSURL *)url {
+    self = [super initWithBaseURL:url];
+    if (!self) {
+        return nil;
+    }
+    
+    [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
+    [self setDefaultHeader:@"Accept" value:@"application/json"];
+    [self setDefaultHeader:@"Content-Type" value:@"application/json"];
+    return self;
+}
+
+#pragma mark - AFIncrementalStore
+
+- (NSURLRequest *)requestForFetchRequest:(NSFetchRequest *)fetchRequest
+                             withContext:(NSManagedObjectContext *)context
+{
+    NSMutableURLRequest *mutableURLRequest = nil;
+    if ([fetchRequest.entityName isEqualToString:@"Tweet"]) {
+        mutableURLRequest = [self requestWithMethod:@"GET" path:@"statuses/public_timeline.json" parameters:nil];
+    }
+    
+    return mutableURLRequest;
+}
+
+- (NSDictionary *)attributesForRepresentation:(NSDictionary *)representation 
+                                         ofEntity:(NSEntityDescription *)entity 
+                                     fromResponse:(NSHTTPURLResponse *)response 
+{
+    NSMutableDictionary *mutablePropertyValues = [[super attributesForRepresentation:representation ofEntity:entity fromResponse:response] mutableCopy];
+    if ([entity.name isEqualToString:@"Tweet"]) {
+        [mutablePropertyValues setValue:[representation valueForKey:@"id"] forKey:@"tweetID"];
+    } else if ([entity.name isEqualToString:@"User"]) {
+        [mutablePropertyValues setValue:[representation valueForKey:@"id"] forKey:@"userID"];
+        [mutablePropertyValues setValue:[representation valueForKey:@"screen_name"] forKey:@"username"];
+        [mutablePropertyValues setValue:[representation valueForKey:@"profile_image_url"] forKey:@"profileImageURLString"];
+    }
+    
+    return mutablePropertyValues;
+}
+
+- (BOOL)shouldFetchRemoteAttributeValuesForObjectWithID:(NSManagedObjectID *)objectID
+                                 inManagedObjectContext:(NSManagedObjectContext *)context
+{
+    return NO;
+}
+
+- (BOOL)shouldFetchRemoteValuesForRelationship:(NSRelationshipDescription *)relationship
+                               forObjectWithID:(NSManagedObjectID *)objectID
+                        inManagedObjectContext:(NSManagedObjectContext *)context
+{
+    return NO;
+}
+
+@end
